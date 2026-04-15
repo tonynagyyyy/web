cfc9f69c06921ebada57a86f002054c89ce00eeb
tonynagyyyy <20235877@fue.edu.eg>
2026-04-15 17:51:25 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <h1 style={{color:"blue"}}>it's lab</h1>
+    </div>
+  );
+}
+
+export default App;
